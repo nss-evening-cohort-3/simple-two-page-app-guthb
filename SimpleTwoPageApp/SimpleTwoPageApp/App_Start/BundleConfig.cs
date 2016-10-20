0969fb1be1def8520fefa7a628c28d3696f73d1d
Bradley Guthrie <guppy96@gmail.com>
2016-10-20 16:11:54 -0500

    angular calculation working

--- SimpleTwoPageApp/SimpleTwoPageApp/App_Start/BundleConfig.cs
+++ SimpleTwoPageApp/SimpleTwoPageApp/App_Start/BundleConfig.cs
@@ -23,6 +23,14 @@ namespace SimpleTwoPageApp
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                     "~/Scripts/angular.js",
+                     "~/Scripts/app.js"));
+                     
+
+
+
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
---
+++ SimpleTwoPageApp/SimpleTwoPageApp/Scripts/app.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("TwoPager", []);
+
+app.controller("AppCtrl", function ($scope) {
+
+    $scope.name = function () {
+
+    }
+
+});
